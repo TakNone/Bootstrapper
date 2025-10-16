61248a450fc2451c77f9ed320cff28f491a7a039
Your New Name <mrtaknone@gmail.com>
2025-10-16 16:21:45 -0700

    Layer 216
    Fix the Layer 216 ( stable version )

--- src/setup.php
+++ src/setup.php
@@ -15,8 +15,10 @@ function setup(string $path) : void {
 	$_ENV['TLPATH'] = $tlpath;
 	Generator::start();
 	$errorsfile = strval($src.DIRECTORY_SEPARATOR.'Errors'.DIRECTORY_SEPARATOR.'errors.json');
-	$errorscontent = json_decode(file_get_contents('http://core.telegram.org/file/400780400470/3/OY6JMkb69K4.143326.json/3c10f72ff9ce45e8a9'),flags : JSON_THROW_ON_ERROR);
-	write($errorsfile,json_encode($errorscontent->descriptions,flags : JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT));
+	if($contents = file_get_contents('http://core.telegram.org/file/400780400470/3/OY6JMkb69K4.143326.json/3c10f72ff9ce45e8a9')){
+		$errorscontent = json_decode($contents,flags : JSON_THROW_ON_ERROR);
+		write($errorsfile,json_encode($errorscontent->descriptions,flags : JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT));
+	}
 }
 
 ?>
